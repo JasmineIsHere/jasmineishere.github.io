66394d2167726717216bbe3270a9a2d4a6cab0a7
Jasmine <jasminetanjm@gmail.com>
2024-07-28 19:12:40 +0800

    add Home page

--- jasmine/src/Router/index.js
+++ jasmine/src/Router/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../components/Home';
+import Work from '../components/Work';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
       </Routes>
     </BrowserRouter>
   );
